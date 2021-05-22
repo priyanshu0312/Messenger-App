63641119ee0abf3eb4523da812c5548ffcc60a8e
mr_sinha03 <Priyanshu>
2021-05-22 13:31:13 +0530

    a

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Chat = () => {
+    return (
+        <div>
+            <h2>Chat</h2>
+        </div>
+    )
+}
+
+export default Chat
---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Join = () => {
+    return (
+        <div>
+            <h1>Join</h1>
+        </div>
+    )
+}
+
+export default Join
